a331114d11b83b1954d1892f3301248a3de66310
DingX2 <pourding@naver.com>
2024-02-08 06:20:19 +0900

    Design: CSS 사이드바 적용

--- BlueMosaic/src/components/Sidebar.tsx
+++ BlueMosaic/src/components/Sidebar.tsx
@@ -5,7 +5,6 @@ import RankingSVG from "../assets/Ranking.svg"
 import MypageSVG from "../assets/Mypage.svg"
 import styled from "@emotion/styled";
 
-
 interface SidebarProps{
   text: string;
   svg: string;
@@ -43,10 +42,12 @@ export const Sidebar = {
     const SVGcomponent = getSVG(svg);
     return(
       <StyledItem onClick={onClick}>
-        <img src={SVGcomponent} alt="SVGcomponent"/>
+      <div>
+        <img src={SVGcomponent} alt="SVGcomponent" />
         <span className="text">{text}</span>
-        <LineGray/>
-      </StyledItem>
+      </div>
+      <LineGray />
+    </StyledItem>
     )
   },
 
@@ -70,59 +71,61 @@ const StyledContainer = styled.div`
 
 const Line = styled.div`
 width: 70%;
-height: 1px;
-flex-shrink: 0;
 display: flex;
 margin: 0 auto;
-stroke-width: 1px;
-stroke: var(--font-color);
+flex-shrink: 0;
+border: 1px solid var(--font-color);
 `
 
 const LineGray = styled.div`
-width: 15.84375rem;
-height: 0.00225rem;
-transform: rotate(-0.008deg);
-flex-shrink: 0;
-stroke-width: 1px;
-stroke: #DBDBDB;
-`
+  width: 70%;
+  flex-shrink: 0;
+  border: 0.5px solid var(--googleGray-color);
+  margin: 0.2rem 0 0 0;
+`;
+
 
 const StyledWrapper = styled.div`
   width: 17.5rem;
-  height: 23.125rem;
+  height: 25rem;
   flex-shrink: 0;
   background: var(--googleWhiteGray-color);
   border-radius: 0rem 0rem 0.625rem 0.625rem;
 `
 
 const StyledItem = styled.div`
-  width: 15rem;
-  height: 1rem;
   display: inline-flex;
-  align-self: stretch;
-  gap: 0.6875rem;
-  display: inline-flex;
-  padding: 0rem 0.75rem;
-  align-items: center;
-
+  flex-direction: column;
+  align-self: stretch;  
+  gap: 0.5rem;
   color: var(--font-color);
   font-family: Itim;
   font-size: 1.125rem;
+  display: flex;
 
-`
+  div {
+    display: flex;
+    display: inline-flex;
+    align-self: stretch;
+    align-items: center;
+    gap: 0.6875rem;
+    width: 100%; 
+  }
+
+  :hover{
+    background-color: #D4D4D4;
+    cursor: pointer; 
+    transition: background-color 0.3s ease; 
+    }
+  }
+`;
 
 const StyledTitle = styled.div`
-width: 100%;
-height: 2.5rem;
-font-family: "Potta One";
-
-color: var(--font-color, #00276F);
-font-family: Itim;
-text-align: center;
-font-family: "Potta One";
-font-size: 1.5rem;
-font-weight: 400;
-
-top: 2.38rem;
-
+  height: 2.5rem;
+  color: var(--font-color, #00276F);
+  font-family: Itim;
+  text-align: center;
+  font-family: "Potta One";
+  font-size: 1.5rem;
+  padding: 2.38rem 0rem 3.30rem;
 `;

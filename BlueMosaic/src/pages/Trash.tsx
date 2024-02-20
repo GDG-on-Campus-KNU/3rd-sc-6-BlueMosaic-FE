c7133e79e3165743e6501df28f59088e980d68f8
DingX2 <pourding@naver.com>
2024-02-20 13:58:41 +0900

    Feat: Trash Page Frame 제작

--- BlueMosaic/src/pages/Trash.tsx
+++ BlueMosaic/src/pages/Trash.tsx
@@ -1,6 +1,43 @@
-export const Trash = () => {
+import styled from "@emotion/styled"
+import { Wrapper, Container} from "../styles/Layout"
+import HomeSVG from "../assets/HomeSVG.svg"
+import WaterWave from 'react-water-wave';
+import PolaroidSVG from "../assets/Polaroid.svg"
+import { useState } from "react"
+import imageUrl from "../assets/UploadBackground.jpg"
 
-  return(<>
-    Trash
-  </>)
+export const Trash = () => {
+  const [showSmartphone, setShowSmartphone] = useState(false);
+
+  const handleClick = () => {
+    setShowSmartphone(true);
+  };
+
+  return(
+    <WaterWave imageUrl={ HomeSVG } style={{ backgroundSize: 'cover' }}>
+      {({ pause, play }) => (
+              <Wrapper>
+                <Container>
+                  
+                  <PolaroidWrapper>
+                    <img src={PolaroidSVG} alt="PolaroidSVG"/>
+                  </PolaroidWrapper>
+                </Container>
+              </Wrapper>
+            )}
+    </WaterWave>
+  )
 }
+
+const PolaroidWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+
+  img {
+    width: 70%;
+    height: 70%;
+  }
+`

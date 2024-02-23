d0099bbedd97431c196f5250e0292ca33338f696
DingX2 <pourding@naver.com>
2024-02-24 02:09:03 +0900

    Feat: Toggle 구현중, 누르면 목록이 보이게

--- BlueMosaic/src/components/dashboard/Achievements.tsx
+++ BlueMosaic/src/components/dashboard/Achievements.tsx
@@ -2,12 +2,22 @@ import styled from "@emotion/styled"
 import { Center } from "../../styles/Layout"
 import { AchievementsApis } from "../../hooks/useAchivementsQuery"
 import { AchievementInfoStore } from "../../stores/AchievementStore"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useStore } from "zustand"
 
 export const Achievements = () => {
 
   const AchievementInfo = useStore(AchievementInfoStore);
+  const [toggleClick, setToggleClick] = useState(false);
+
+  const handleToggle = (index: number) => {
+    AchievementInfo.toggle(index);
+    console.log(index);
+  };
+
+  const handleToggleClick = () => {
+    setToggleClick(!toggleClick);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,13 +51,13 @@ export const Achievements = () => {
         if (value === true) {
           const cleanedKey = key.replace(/\d/g, '');
           if (cleanedKey.includes("Cleaner")) {
-            cleanerAchievements.push(cleanedKey);
+            cleanerAchievements.unshift(cleanedKey);
           } else if (cleanedKey.includes("Diver")) {
-            diverAchievements.push(cleanedKey);
+            diverAchievements.unshift(cleanedKey);
           } else if (cleanedKey.includes("Photographer")) {
-            photographerAchievements.push(cleanedKey);
+            photographerAchievements.unshift(cleanedKey);
           } else if (cleanedKey.includes("Pioneer")) {
-            pioneerAchievements.push(cleanedKey);
+            pioneerAchievements.unshift(cleanedKey);
           }
         }
       });
@@ -70,26 +80,33 @@ export const Achievements = () => {
   return (
     <AchievementsWrapper>
       {AchievementInfoStore.getState().cleaner[0] && (
-        <AchievementsFish color="--googleblue-color">
-          <span>{"experiencedCleaner012"}</span>
+        <AchievementsFish color="--googleblue-color" Click={handleToggleClick}>
+          {toggleClick ? (
+            <span>{AchievementInfoStore.getState().cleaner[AchievementInfoStore.getState().select[0]]}</span>
+          ) : (
+            AchievementInfoStore.getState().cleaner.map((item, index) => (
+              <span key={index} onClick={() => handleToggle(index)}>{item}</span>
+            ))
+          )}
         </AchievementsFish>
       )}
 
+
       {AchievementInfoStore.getState().diver[0] || (
         <AchievementsFish color="--googleyellow-color">
-          <span>{"dolphin"}</span>
+          <span>{AchievementInfoStore.getState().diver[AchievementInfoStore.getState().select[1]]}</span>
         </AchievementsFish>
       )}
 
       {AchievementInfoStore.getState().photographer[0] || (
         <AchievementsFish color="--googleRed-color">
-          <span>{"novicePhotographer"}</span>
+          <span>{AchievementInfoStore.getState().photographer[AchievementInfoStore.getState().select[2]]}</span>
         </AchievementsFish>
       )}
 
       {AchievementInfoStore.getState().pioneer[0] || (
         <AchievementsFish color="--googlegreen-color">
-          <span>{"pioneer"}</span>
+          <span>{AchievementInfoStore.getState().pioneer[AchievementInfoStore.getState().select[3]]}</span>
         </AchievementsFish>
       )}
     </AchievementsWrapper>
@@ -101,10 +118,9 @@ export const Achievements = () => {
 export const AchievementsFish = styled.section`
   max-height: 100%;
   display: flex;
+  flex-direction: column;
   flex-shrink: 0;
   align-self: stretch;
-  ${Center}
-  padding: 0.33rem 1.25rem;
   border-radius: 1.25rem;
   border: 2px solid var(--googleblue-color);
   background: var(--white-color);
--- BlueMosaic/src/stores/AchievementStore.ts
+++ BlueMosaic/src/stores/AchievementStore.ts
@@ -1,16 +1,19 @@
 import { create } from "zustand";
-import { devtools, persist } from "zustand/middleware";
+import { devtools } from "zustand/middleware";
 
 export interface AchievementInfo {
   cleaner: string[];
   diver: string[];
   photographer: string[];
   pioneer: string[];
+  select: number[]; // [Cleaner, Diver, Photographer, Pioneer] Index
 
   setCleaner: (cleaner: AchievementInfo['cleaner']) => void;
   setDiver: (diver: AchievementInfo['diver']) => void;
   setPhotographer: (photographer: AchievementInfo['photographer']) => void;
   setPioneer: (pioneer: AchievementInfo['pioneer']) => void;
+  setSelect: (select: AchievementInfo['select']) => void;
+  toggle: (index: number) => void;
 }
 
 const createAchievementInfoStore = (set) => ({
@@ -18,16 +21,26 @@ const createAchievementInfoStore = (set) => ({
   diver: [""],
   photographer: [""],
   pioneer: [""],
+  select: [0, 0, 0, 0], // 각 항목에 대한 인덱스
 
   setCleaner: (cleaner: AchievementInfo['cleaner']) => set({ cleaner }),
   setDiver: (diver: AchievementInfo['diver']) => set({ diver }),
   setPhotographer: (photographer: AchievementInfo['photographer']) => set({ photographer }),
   setPioneer: (pioneer: AchievementInfo['pioneer']) => set({ pioneer }),
+  setSelect: (select: AchievementInfo['select']) => set({ select }),
+  toggle: (index: number) => {
+    set((state) => {
+      const newSelect = [...state.select];
+      const selected = newSelect.indexOf(index);
+      newSelect[selected] = 1 - newSelect[selected];
+      return { select: newSelect };
+    });
+  },
 });
 
 let AchievementInfoStoreTemp;
 
-//devtools
+// devtools
 if (import.meta.env.DEV) {
   AchievementInfoStoreTemp = create<AchievementInfo>()(devtools(createAchievementInfoStore, { name: 'AchievementInfo' }));
 } else {
